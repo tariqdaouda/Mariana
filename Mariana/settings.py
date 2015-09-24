17f4e715971a9bfac5a2a6d3df2829039f004463
Jonathan Séguin <jo@jonathanseguin.com>
2015-09-24 10:56:43 -0400

    Do not automatically create and open log file

--- Mariana/settings.py
+++ Mariana/settings.py
@@ -4,7 +4,8 @@ OMICRON_SIGNATURE = ">|\/| /-\ |-> | /-\ |\| /-\>"
 VERBOSE = False
 SAVE_MESSAGE_LOG = False
 SAVE_MESSAGE_LOG_FILE = "Mariana_logs.txt"
-MESSAGE_LOG_FILE = open(SAVE_MESSAGE_LOG_FILE, "w")
+if SAVE_MESSAGE_LOG:
+    MESSAGE_LOG_FILE = open(SAVE_MESSAGE_LOG_FILE, "w")
 
 AUTOCAST = True #attempt to cast inputs to float32 if theano raises a TypeError
 #The random used for initialising weights
