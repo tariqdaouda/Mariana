7ba20852eb989299579cb465804a3f5532c814d3
Tariq Daouda <tariq.daouda@umontreal.ca>
2015-11-16 17:54:20 -0500

    removed factor from ABC constructor

--- Mariana/regularizations.py
+++ Mariana/regularizations.py
@@ -3,10 +3,8 @@ __all__ = ["SingleLayerRegularizer_ABC", "L1", "L2"]
 class SingleLayerRegularizer_ABC(object) :
 	"""An abstract regularization to be applied to a layer."""
 
-	def __init__(self, factor, *args, **kwargs) :
+	def __init__(self, *args, **kwargs) :
 		self.name = self.__class__.__name__
-		self.factor = factor
-		self.hyperparameters = ["factor"]
 
 	def getFormula(self, layer) :
 		"""Returns the expression to be added to the cost"""
@@ -19,9 +17,12 @@ class L1(SingleLayerRegularizer_ABC) :
 
 			factor * abs(Weights)
 	"""
+	def __init__(self, factor) :
+		SingleLayerRegularizer_ABC.__init__(self)
+		self.factor = factor
+		self.hyperparameters = ["factor"]
 
 	def getFormula(self, layer) :
-		# print "ssss L1" 
 		return self.factor * ( abs(layer.W).sum() )
 
 class L2(SingleLayerRegularizer_ABC) :
@@ -31,7 +32,10 @@ class L2(SingleLayerRegularizer_ABC) :
 
 			factor * (Weights)^2
 	"""
+	def __init__(self, factor) :
+		SingleLayerRegularizer_ABC.__init__(self)
+		self.factor = factor
+		self.hyperparameters = ["factor"]
 
 	def getFormula(self, layer) :
-		# print "pppp L2" 
 		return self.factor * ( (layer.W * layer.W).sum() )
