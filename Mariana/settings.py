d21bd92d99354f457b81030c64902301ea5cb12e
Tariq Daouda <tariq.daouda@umontreal.ca>
2016-08-26 19:59:54 -0400

    saved logs is default behaviour

--- Mariana/settings.py
+++
@@ -1,9 +0,0 @@
-#Personal tribute to nomad soul
-OMICRON_SIGNATURE = ">|\/| /-\ |-> | /-\ |\| /-\>"
-
-VERBOSE = True
-SAVE_MESSAGE_LOG = False
-SAVE_MESSAGE_LOG_FILE = "Mariana_logs.txt"
-
-#The random used for initialising weights
-RANDOM_SEED = None
