c6b228867a19dac50bfd35de49e2422e9b8a39d6
Tariq Daouda <tariq.daouda@umontreal.ca>
2016-10-14 16:50:39 -0400

    Bring back settings.py

---
+++ Mariana/settings.py
@@ -0,0 +1,9 @@
+#Personal tribute to nomad soul
+OMICRON_SIGNATURE = ">|\/| /-\ |-> | /-\ |\| /-\>"
+
+VERBOSE = True
+SAVE_MESSAGE_LOG = True
+SAVE_MESSAGE_LOG_FILE = "Mariana_logs.txt"
+
+#The random used for initialising weights
+RANDOM_SEED = None
