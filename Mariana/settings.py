103225d30e62313ef38f41fadae58bfa66e365c5
Tariq Daouda <tariq.daouda@umontreal.ca>
2016-10-17 14:15:02 -0400

    Update settings.py

--- Mariana/settings.py
+++ Mariana/settings.py
@@ -2,7 +2,7 @@
 OMICRON_SIGNATURE = ">|\/| /-\ |-> | /-\ |\| /-\>"
 
 VERBOSE = True
-SAVE_MESSAGE_LOG = False
+SAVE_MESSAGE_LOG = True
 SAVE_MESSAGE_LOG_FILE = "Mariana_logs.txt"
 
 #The random used for initialising weights
